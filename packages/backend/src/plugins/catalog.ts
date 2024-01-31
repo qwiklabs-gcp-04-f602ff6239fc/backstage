48207ac28fbfcc2fca893c520d5d29bf179f8572
Ada Lovelace <ada@example.com>
2024-01-31 19:29:51 +0000

    chore: replace values

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -14,7 +14,7 @@ export default async function createPlugin(
   builder.addEntityProvider(
     GithubOrgEntityProvider.fromConfig(env.config, {
       id: 'production',
-      orgUrl: "https://github.com/${{__GITHUB_ORG__}}",
+      orgUrl: "https://github.com/qwiklabs-gcp-04-f602ff6239fc",
       logger: env.logger,
       schedule: env.scheduler.createScheduledTaskRunner({
         frequency: { minutes: 3 },
